e94cbce839cc79c26c9307833940f36192ed0c64
Wrong <ilak71363@gmail.com>
2022-05-18 15:42:36 +0300

    Добавьте файлы проекта.

---
+++ IntersheepDeeplay/ViewModel/MainVM.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace IntersheepDeeplay.ViewModel
+{
+    public class MainVM : INotifyPropertyChanged
+    {
+        public string gridWidth;
+        private bool IsShowPanel;
+        private RelayCommand hideShowPanel;
+        private void ShowPanel()
+        {
+            gridWidth = "0.3*";
+            IsShowPanel = true;
+        }
+        private void HidePanel()
+        {
+            gridWidth = "0";
+            IsShowPanel = false;
+        }
+
+        public MainVM()
+        {
+            IsShowPanel = true;  
+        }
+
+        public string GridWidth
+        {
+            get { return gridWidth; }
+        }
+            
+        public RelayCommand HideShowPanel
+        {
+            get
+            {
+                return hideShowPanel ?? (hideShowPanel = new RelayCommand((FindWidth) =>
+                {
+                    if (IsShowPanel == true)
+                        HidePanel();
+                    else ShowPanel();
+                }));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        
+        public void OnPropertyChanged([CallerMemberName] string prop = " ")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
